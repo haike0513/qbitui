31d81dd8112aba10f673a491edfd359511b05fdc
haike <haike0513@gmail.com>
2022-03-30 11:03:04 +0800

    add sdk-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { RootRouter } from '@qbitui/router';
+import Api from '@qbitapi/sdk-api';
+import { useEffect } from 'react';
 
 
 
 function App() {
+  useEffect(() => {
+    console.log(Api);
+  }, []);
 
   return (
     <>
