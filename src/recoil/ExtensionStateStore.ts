b579310257a0da389580bc4a52e72dff71ff2b2d
haike <haike0513@gmail.com>
2022-03-30 08:55:19 +0800

    add build tag workflow

--- src/recoil/ExtensionStateStore.ts
+++
@@ -1,8 +0,0 @@
-import type { InjectedExtension } from '@polkadot/extension-inject/types';
-import { atom, RecoilState } from 'recoil';
-
-// type InjectedAccountWithMeta = any;
-export const currentInjectedExtenstionState = atom<InjectedExtension | undefined>({
-  key: 'currentInjectedExtenstionState', // unique ID (with respect to other atoms/selectors)
-  default: undefined,
-});
